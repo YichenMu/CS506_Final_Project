bc0f58146e22a1016beefdef76c0f96352156a79
Joey Cheng <997232782@qq.com>
2021-04-05 10:32:28 -0400

    debug for aggregate_clustering.py

--- code/aggregate_clustering.py
+++ code/aggregate_clustering.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
 def faneuil_hall():
-    file = ["FANEIUL_HALL_MERCHANTS_ASSOC.csv",
-            "FANEUIL_HALL_MERCHANT_ASSOC_.csv",
-            "FANEUIL_HALL_MERCHANTS_ASSOC.csv",
-            "FANEUIL_HALL_MERCHANTS_ASSOC_.csv",
-            "FANUEIL_HALL_MERCHANT_ASSOCIAT.csv"]
+    # file = ["FANEIUL_HALL_MERCHANTS_ASSOC.txt",
+    #         "FANEUIL_HALL_MERCHANT_ASSOC_.csv",
+    #         "FANEUIL_HALL_MERCHANTS_ASSOC.csv",
+    #         "FANEUIL_HALL_MERCHANTS_ASSOC_.csv",
+    #         "FANUEIL_HALL_MERCHANT_ASSOCIAT.csv"]
 
 
-    fan0 = pd.read_csv("../data/event_type_clusterings/" + file[0])
-    fan1 = pd.read_csv("../data/event_type_clusterings/" + file[1])
-    fan2 = pd.read_csv("../data/event_type_clusterings/" + file[2])
-    fan3 = pd.read_csv("../data/event_type_clusterings/" + file[3])
-    fan4 = pd.read_csv("../data/event_type_clusterings/" + file[4])
+    fan0 = pd.read_csv("../data/event_type_clusterings/FANEUIL_HALL_MERCHANTS_ASSOC")
+    fan1 = pd.read_csv("../data/event_type_clusterings/FANEUIL_HALL_MERCHANT_ASSOC_")
+    fan2 = pd.read_csv("../data/event_type_clusterings/FANEUIL_HALL_MERCHANTS_ASSOC")
+    fan3 = pd.read_csv("../data/event_type_clusterings/FANEUIL_HALL_MERCHANTS_ASSOC_")
+    fan4 = pd.read_csv("../data/event_type_clusterings/FANUEIL_HALL_MERCHANT_ASSOCIAT")
 
     df = pd.concat([fan0, fan1, fan2, fan3, fan4])
 
