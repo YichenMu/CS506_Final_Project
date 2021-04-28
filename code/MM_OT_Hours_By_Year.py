2459fbd19f5512d2d7e4939b3668348af7f41030
Joey Cheng <997232782@qq.com>
2021-04-28 16:48:06 -0400

    Added OT Hours for MM by year plot

--- code/MM_OT_Hours_By_Year.py
+++ code/MM_OT_Hours_By_Year.py
@@ -2,11 +2,24 @@
 # Date: 2021 / 4 / 26
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-OTHours = []
+OTHours, years = [], []
 
 for i in range(2015, 2021):
     data = pd.read_csv("../data/Special-Events-" + str(i) + ".csv")
     OTHours.append(data.loc[data["DESCRIPTION"] == "MASS / MELNEA"]["OTHOURS"].sum())
+    years.append(i)
 
-print(OTHours)
+bars = plt.barh(years, OTHours, color=["sienna", "olive", "brown", "peru"])
+
+# add annotates
+for bar, label in zip(bars, OTHours):
+    plt.annotate(label, xy=(label, bar.get_y() + 0.4), ha='left', va='center')
+
+plt.title("OT Hours for Mass/Melnea by Year")
+plt.ylabel("Year")
+plt.xlabel("OT Hours")
+plt.xlim((0, 48000))
+plt.tight_layout()
+plt.savefig("../img/OT Hours for MassMelnea by Year.png")
